6cd9bc76d870d401527eac5d44c02830bebc5ec5
jinglion <cjhjinglion0529@163.com>
2015-08-18 18:25:39 +0800

    第三次提交，增加Broadercast

---
+++ app/src/main/java/com/example/jinglion/firsttest/Broadcast/BootBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package com.example.jinglion.firsttest.Broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by jinglion on 2015-8-18.
+ */
+public class BootBroadcastReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+}
